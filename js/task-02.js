a49aa0889257bfa4cf56f3e544176cdd0c20f6b8
Dmytro Stupak <stupakbf@gmail.com>
2022-08-04 23:03:27 +0300

    1, 2, 3(+/-) tasks done

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +8,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let obj = document.getElementById('ingredients')
+
+const elements = ingredients.map(options => {
+const newCatergorieItem = document.createElement(`li`);
+  newCatergorieItem.classList.add(`item`);
+  newCatergorieItem.textContent = options;
+  
+  return newCatergorieItem;
+})
+
+obj.append(...elements);
+
+
+console.log(obj);
+// console.log(obj.children);
+// console.log(elements.map( h=> h.textContent ));
+
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,48 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryList = document.querySelector(".gallery");
+
+const newGallery = images.map(pics => {
+  
+  const newGalleryItem = ({ url, alt }) =>   
+    `<li><img src="${url}" alt="${alt}" width = 180 height = 130></li>`; 
+  // const newPic = document.createElement(`img`)
+  // // newPic.src = pics;
+  // // newGalleryItem.append(newPic)
+  // return newGalleryItem + newPic
+})
+galleryList.insertAdjacentHTML("afterbegin", newGallery);
+console.log(galleryList)
+
+// const galleryList = document.querySelector(".gallery");
+// const newGallery = images.map(pics => {
+//   const newGalleryItem = document.createElement(`li`);
+//   const newPics = document.createElement(`img`)
+
+//   newPics.src = pics.url;
+//   newPics.alt = pics.alt;
+//   newPics.width = 180;
+//   newPics.height = 130;
+
+//   newGalleryItem.append(newPics);
+//   console.log(newGalleryItem)
+//   // console.log(newPics)
+  
+//   return newGalleryItem
+  
+// })
+
+// galleryList.append(...newGallery); //work!!!
+
+
+// const createGalleryItem = ({ url, alt }) =>
+//   `<li><img src="${url}" alt="${alt}" width = 180 height = 130></li>`;
+// const galleryMarkup = images.reduce(
+//   (acc, item) => acc + createGalleryItem(item),
+//   ""
+// );
+// const galleryList = document.querySelector("#gallery");
+// galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+// galleryList.setAttribute("style", "list-style-type:none; display: flex;");
